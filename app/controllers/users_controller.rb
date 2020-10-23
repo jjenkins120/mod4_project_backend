30c334f5377d86a9669b476a53ded026fa458be3
jjenkins120 <jjenkins120@gmail.com>
2020-10-22 20:07:18 -0500

    newest commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'JWT'
+
 class UsersController < ApplicationController
     wrap_parameters :user, include: [:username, :password, :email, :first_name, :last_name]
 
@@ -32,11 +34,26 @@ class UsersController < ApplicationController
     def login
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
+            payload = { user_id: @user.id}
+            token = JWT.encode(payload, 'S3cr3t', 'HS256')
+
             render json: @user
         else 
             render json: {error: "Spin again, friend. Invalid username or password"}
         end
     end
+
+    def current_user
+        token = request.headers[:Authorization].split(' ')[1]
+        decoded_token = JWT.decode(token, 'S3cr3t', true, { algorithm: 'HS256' })
+        user_id = decoded_token[0]['user_id']
+    
+    
+        user = User.find(user_id)
+    
+        render json: { user: user }
+    
+      end
 
     private
 
