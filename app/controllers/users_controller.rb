6d3e07ab246d5d1a5b8064d765b8a8d4f76f45ac
jjenkins120 <jjenkins120@gmail.com>
2020-10-23 12:22:38 -0500

    newest commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
             payload = { user_id: @user.id}
             token = JWT.encode(payload, 'S3cr3t', 'HS256')
 
-            render json: @user
+            render json: {user: @user, token: token}
         else 
             render json: {error: "Spin again, friend. Invalid username or password"}
         end
@@ -47,7 +47,6 @@ class UsersController < ApplicationController
         token = request.headers[:Authorization].split(' ')[1]
         decoded_token = JWT.decode(token, 'S3cr3t', true, { algorithm: 'HS256' })
         user_id = decoded_token[0]['user_id']
-    
     
         user = User.find(user_id)
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   
 post '/users/sessions/login', to: 'users#login'
-get '/users/current_user', to: 'users#current_user'
+get '/users/sessions/current_user', to: 'users#current_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
